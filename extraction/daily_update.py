b78d48c7d9970d5140ca8a61a305178947e02674
Antuel Quirino <antuel.quirino@gmail.com>
2026-04-06 21:12:51 +0200

    Fix script name in github action

--- extraction/daily_update.py
+++ extraction/daily_update.py
@@ -13,19 +13,18 @@ TICKERS = ["AAPL", "AMZN", "NVDA", "XOM", "CVX", "JPM", "C", "^GSPC"]
 client = bigquery.Client(project=PROJECT_ID)
 
 def run_incremental_extraction():
-    # Bajamos los últimos 5 días para cubrir fines de semana o feriados
+    
     print(f"📥 Descargando precios recientes para: {TICKERS}")
     df = yf.download(TICKERS, period="5d", interval="1d", auto_adjust=True)
     
-    # Formatear el DataFrame de Yahoo (Close prices)
+    
     df = df['Close'].stack().reset_index()
     df.columns = ['date', 'ticker', 'close']
     
-    # Limpiar la fecha (quitar zona horaria para BigQuery)
+    
     df['date'] = pd.to_datetime(df['date']).dt.tz_localize(None)
 
-    # CONFIGURACIÓN DE CARGA: 
-    # Usamos WRITE_APPEND para que se sumen a la tabla existente
+    
     job_config = bigquery.LoadJobConfig(
         write_disposition="WRITE_APPEND",
     )
